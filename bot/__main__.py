737130d6359672d1acba2454660ad4b512b2cd51
Valentin B <valentin.be@protonmail.com>
2020-11-13 20:37:18 +0100

    Fix pipenv and make it run

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from 
+from .squadbot import Bot
 
 bot = Bot()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.run()
--- bot/squadbot.py
+++ bot/squadbot.py
@@ -1,18 +1,17 @@
-import discord
 from discord.ext import commands
 
+
 class Bot(commands.Bot):
+    def __init__(self):
+        pass
 
-  def __init__(self):
-    pass
+    def run(self):
+        pass
 
-  def run(self):
-    pass
-    
-  async def on_connect(self):
-    pass
-    
-  async def on_ready(self):
-      print(f'Logged in as {self.user.name}')
-      print(f'ID: {self.user.id}')
-      print(f'Owner: {self.owner}')
+    async def on_connect(self):
+        pass
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user.name}")
+        print(f"ID: {self.user.id}")
+        print(f"Owner: {self.owner}")
