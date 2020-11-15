1daa7bb7c6ac8baf3062a90bec1105b09c9c2409
Valentin B <valentin.be@protonmail.com>
2020-11-15 16:47:05 +0100

    Add helper method to upsert Autistenpunkte score

--- bot/db/schema/autistenpunkte.py
+++ bot/db/schema/autistenpunkte.py
@@ -1,3 +1,6 @@
+import discord
+from sqlalchemy.dialects.postgresql import insert
+
 from .. import DATABASE as db
 
 
@@ -6,3 +9,15 @@ class Autistenpunkte(db.Model):
 
     user_id = db.Column(db.BigInteger(), primary_key=True)
     score = db.Column(db.Integer())
+
+    @staticmethod
+    async def insert_or_increase_score(user: discord.User, score: int):
+        # Build a query that either inserts the score if no column for the user
+        # exists yet or increases the score a user already has by the given value.
+        user_insert = insert(Autistenpunkte).values(user_id=user.id, score=score)
+        user_insert = user_insert.on_conflict_do_update(
+            index_elements=[Autistenpunkte.user_id],
+            set_=dict(score=Autistenpunkte.score + score),
+        )
+
+        await user_insert.gino.model(Autistenpunkte).first()
