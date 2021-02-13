f82c7d08c5230324fe07cfcae7b7c3993714f596
Erik1000 <erik1000@protonmail.com>
2021-02-13 17:53:42 +0100

    init backend

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from bot.db import connection
+from bot.utils.config import BOT_CONFIG
+
+app = FastAPI(
+    title="Spastenbude Bot",
+    description="The bot of vales suicide squad.",
+    docs_url="/api/docs",
+    redoc_url=None,
+    openapi_url="/api/openapi.json",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=BOT_CONFIG.backend_origins_allowed,
+    allow_credentials=BOT_CONFIG.backend_origins_allow_credentials,
+    allow_methods=BOT_CONFIG.backend_origins_allowed_methods,
+    allow_headers=BOT_CONFIG.backend_origins_allowed_headers,
+    expose_headers=BOT_CONFIG.backend_origins_exposed_headers,
+    max_age=BOT_CONFIG.backend_origins_max_age,
+)
+
+
+@app.on_event("startup")
+async def startup_event():
+    await connection.init_connection()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await connection.close_connection()
--- bot/utils/config.py
+++ bot/utils/config.py
@@ -14,6 +14,24 @@ class Config(BaseSettings):
     postgres_port: str = Field(None, env="POSTGRES_PORT")
     postgres_db_url: str = Field(None, env="POSTGRES_DB_URL")
 
+    backend_origins_allowed: Set[str] = Field([], env="BACKEND_ORIGINS_ALLOWED")
+    backend_origins_allow_credentials: bool = Field(
+        True, env="BACKEND_ORIGINS_ALLOW_CREDENTIALS"
+    )
+    backend_origins_allowed_methods: Set[str] = Field(
+        ["*"], env="BACKEND_ORIGINS_ALLOWED_METHODS"
+    )
+    backend_origins_allowed_headers: Set[str] = Field(
+        ["*"], env="BACKEND_ORIGINS_ALLOWED_HEADERS"
+    )
+    backend_origins_exposed_headers: Set[str] = Field(
+        [], env="BACKEND_ORIGINS_EXPOSED_HEADERS"
+    )
+    backend_discord_oauth2_url: str = Field(
+        "https://discord.com/api/oauth2/authorize", env="BACKEND_DISCORD_OAUTH2_URL"
+    )
+    backend_origins_max_age: int = Field(600, env="BACKEND_ORIGINS_MAX_AGE")
+
     def construct_database_url(self) -> str:
         if url := self.postgres_db_url:
             return url
