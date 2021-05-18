ef835d08e94c0a0609b6e76c93552cfb37d63fcb
Erik Tesar <erik@erik-tesar.com>
2021-05-18 11:38:46 +0200

    add column for invite code used

--- bot/db/schema/invite.py
+++ bot/db/schema/invite.py
@@ -5,3 +5,4 @@ class InvitedMember(db.Model):
     __tablename__ = "invited_members"
     member_id: int = db.Column(db.BigInteger(), primary_key=True)
     inviter: int = db.Column(db.BigInteger(), nullable=False)
+    invite: str = db.Column(db.String(), nullable=False)
